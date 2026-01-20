b4b038ad61c63874de8d44e00557597c0cdbde59
Owen Werts <bninja@byu.edu>
2026-01-20 13:30:17 -0700

    Added a list of hours as strings to main.go to be used by week view table to generate grid.

--- main.go
+++ main.go
@@ -15,13 +15,15 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	templateSet, err := template.ParseFiles(pages...)
+	hours := []string{"8:00", "9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00"}
+
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
-	err = templateSet.ExecuteTemplate(w, "base", nil)
+	err = templateSet.ExecuteTemplate(w, "base", hours)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -39,11 +41,13 @@ func createPost(w http.ResponseWriter, r *http.Request) {
 func main() {
 	// Servemux is the same as a React Browser Router
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./templates/static/"))
+
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	// Route Declarations
 	mux.HandleFunc("GET /", home)
 	mux.HandleFunc("POST /create", createPost)
-	mux.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.Dir("templates/static"))))
 
 	// Start server at http://localhost:4444
 	log.Print("Starting Server on port 4444")
